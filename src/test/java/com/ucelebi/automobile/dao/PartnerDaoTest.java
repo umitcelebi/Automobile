33471309c25871b72f94a4f73dbbd6d8d6d44e47
umitcelebi <umitcelebi2035@gmail.com>
2023-06-08 14:51:31 +0300

    edited PartnerDaoTest class

--- src/test/java/com/ucelebi/automobile/dao/PartnerDaoTest.java
+++ src/test/java/com/ucelebi/automobile/dao/PartnerDaoTest.java
@@ -2,7 +2,10 @@ package com.ucelebi.automobile.dao;
 
 import com.ucelebi.automobile.dto.PartnerListDTO;
 import com.ucelebi.automobile.enums.Role;
+import com.ucelebi.automobile.model.Address;
+import com.ucelebi.automobile.model.City;
 import com.ucelebi.automobile.model.Partner;
+import com.ucelebi.automobile.model.Town;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -36,6 +39,13 @@ class PartnerDaoTest {
 
     @Autowired
     private PartnerDao underTest;
+
+    @Autowired
+    private CityDao cityDao;
+    @Autowired
+    private TownDao townDao;
+    @Autowired
+    private AddressDao addressDao;
 
     @Test
     void itShouldSavePartnerSuccessfully() {
@@ -297,4 +307,133 @@ class PartnerDaoTest {
                 .isEqualTo(partner2);
     }
 
+    @Test
+    void itShouldSelectPartnerByCityOrderByDistance() {
+        //Given
+        City city = new City("34","Istanbul",null);
+        City city2 = new City("55","Samsun",null);
+        cityDao.saveAll(List.of(city,city2));
+
+        Address address = new Address("ADDR1","34233",null,city,null,"sokak adı","numaralı sokak Daire: 1","Ek Açıklama");
+        Address address2 = new Address("ADDR2","55233",null,city2,null,"sokak adı","numaralı sokak Daire: 1","Ek Açıklama");
+        addressDao.saveAll(List.of(address, address2));
+
+        Partner partner1 = new Partner.builder()
+                .uid("prestigeAuto")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .role(Role.PARTNER)
+                .phoneNumber("05436567786")
+                .latitude(14.265)
+                .longitude(13.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        Partner partner2 = new Partner.builder()
+                .uid("prestigeAuto2")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .role(Role.PARTNER)
+                .address(address2)
+                .phoneNumber("05436567786")
+                .latitude(45.265)
+                .longitude(23.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        Partner partner3 = new Partner.builder()
+                .uid("prestigeAuto3")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .address(address)
+                .role(Role.PARTNER)
+                .phoneNumber("05436567786")
+                .latitude(58.265)
+                .longitude(23.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        List<Partner> partners = Arrays.asList(partner1, partner2, partner3);
+
+        underTest.saveAll(partners);
+
+        //When
+        Page<PartnerListDTO> partnersByDistance = underTest.findByAddress_City_CodeByDistance(45.300, 25.444, "34",PageRequest.of(0, 5));
+
+        //Then
+        assertEquals(1,partnersByDistance.getTotalElements());
+
+        // distance of partner2 is closest
+        assertThat(partnersByDistance.getContent().get(0)).usingRecursiveComparison()
+                .ignoringFields("distance")
+                .isEqualTo(partner3);
+    }
+
+    @Test
+    void itShouldSelectPartnerByTownOrderByDistance() {
+        //Given
+        Town town = new Town("3406","Güngören",null);
+        Town town2 = new Town("3407","Salıpazarı",null);
+        townDao.saveAll(List.of(town2, town));
+
+        Address address = new Address("ADDR1","34233",null,null,town,"sokak adı","numaralı sokak Daire: 1","Ek Açıklama");
+        Address address2 = new Address("ADDR2","55233",null,null,town2,"sokak adı","numaralı sokak Daire: 1","Ek Açıklama");
+        addressDao.saveAll(List.of(address, address2));
+
+        Partner partner1 = new Partner.builder()
+                .uid("prestigeAuto")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .role(Role.PARTNER)
+                .phoneNumber("05436567786")
+                .latitude(14.265)
+                .longitude(13.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        Partner partner2 = new Partner.builder()
+                .uid("prestigeAuto2")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .role(Role.PARTNER)
+                .address(address2)
+                .phoneNumber("05436567786")
+                .latitude(45.265)
+                .longitude(23.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        Partner partner3 = new Partner.builder()
+                .uid("prestigeAuto3")
+                .name("Prestige Auto")
+                .displayName("Prestige Auto")
+                .address(address)
+                .role(Role.PARTNER)
+                .phoneNumber("05436567786")
+                .latitude(58.265)
+                .longitude(23.635)
+                .sundayOpen(false)
+                .password("pass123456")
+                .build();
+
+        List<Partner> partners = Arrays.asList(partner1, partner2, partner3);
+
+        underTest.saveAll(partners);
+
+        //When
+        Page<PartnerListDTO> partnersByDistance = underTest.findByAddress_Town_CodeByDistance(45.300, 25.444, "3407",PageRequest.of(0, 5));
+
+        //Then
+        assertEquals(1,partnersByDistance.getTotalElements());
+
+        // distance of partner2 is closest
+        assertThat(partnersByDistance.getContent().get(0)).usingRecursiveComparison()
+                .ignoringFields("distance")
+                .isEqualTo(partner2);
+    }
 }
