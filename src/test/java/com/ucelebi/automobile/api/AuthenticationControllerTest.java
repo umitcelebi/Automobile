b5ebca6dc450f38a442981544dceb30104cd6e02
umitcelebi <umitcelebi2035@gmail.com>
2023-06-18 23:13:56 +0300

    added AuthenticationControllerTest class

---
+++ src/test/java/com/ucelebi/automobile/api/AuthenticationControllerTest.java
@@ -0,0 +1,22 @@
+package com.ucelebi.automobile.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * User: ucelebi
+ * Date: 18.06.2023
+ * Time: 23:10
+ */
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthenticationControllerTest {
+    @Test
+    void itShouldRegisterSuccessfully() {
+        //Given
+        //When
+        //Then
+    }
+}
